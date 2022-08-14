ef51f36f33d7b58caa8a9e4980395614391672dd
Tarek <tarek.shehabeldin.ibrahim@gmail.com>
2022-08-14 23:58:58 +0200

    initial commit

---
+++ dist/index.bba0b65b.js
@@ -0,0 +1,5 @@
+/* Global Variables */ // Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+//# sourceMappingURL=index.bba0b65b.js.map
